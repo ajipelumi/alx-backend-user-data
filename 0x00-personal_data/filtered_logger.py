38ddbb4bacdb98282c6957a01ce02e0b66882e6c
ajipelumi <ajisafeoluwapelumi@gmail.com>
2023-08-05 00:04:23 +0100

    created connector

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -60,7 +60,7 @@ def get_logger() -> logging.Logger:
 
 def get_db() -> mysql.connector.connection.MySQLConnection:
     """ Returns a connector to a database. """
-    connector = mysql.connector.connect(
+    connector = mysql.connector.connection.MySQLConnection(
         user=os.environ.get('PERSONAL_DATA_DB_USERNAME', 'root'),
         password=os.environ.get('PERSONAL_DATA_DB_PASSWORD', ''),
         host=os.environ.get('PERSONAL_DATA_DB_HOST', 'localhost'),
