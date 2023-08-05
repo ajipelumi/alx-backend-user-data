c5b07036b813ce6a37a0a1bbb56848a254fae4e7
ajipelumi <ajisafeoluwapelumi@gmail.com>
2023-08-05 21:51:23 +0100

    validated password

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -11,3 +11,9 @@ def hash_password(password: str) -> bytes:
     hashed = bcrypt.hashpw(password.encode('utf-8'), salt)
     # Return the hashed password
     return hashed
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """ Validates a password. """
+    # Check if the password is valid
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
