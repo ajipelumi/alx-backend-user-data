94eb313937958505588d17867580cc0dc0e99610
ajipelumi <ajisafeoluwapelumi@gmail.com>
2023-08-08 18:54:04 +0100

    handled base64 extraction

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -5,4 +5,16 @@ from api.v1.auth.auth import Auth
 
 class BasicAuth(Auth):
     """ Basic Auth class. """
-    pass
+    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+        """ Extract base64 authorization header. """
+        # Check if authorization_header is None.
+        if authorization_header is None:
+            return None
+        # Check if authorization_header is not a string.
+        if not isinstance(authorization_header, str):
+            return None
+        # Check if authorization_header does not start with Basic.
+        if not authorization_header.startswith('Basic '):
+            return None
+        # Return authorization_header without Basic.
+        return authorization_header[6:]
