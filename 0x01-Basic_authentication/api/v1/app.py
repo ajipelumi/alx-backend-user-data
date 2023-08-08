cc307278cb018fe4b35f95ff8d7ec68309a464b6
ajipelumi <ajisafeoluwapelumi@gmail.com>
2023-08-08 18:40:09 +0100

    handled authentication

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -12,6 +12,10 @@ import os
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+auth = None
+if os.getenv('AUTH_TYPE') == 'auth':
+    from api.v1.auth.auth import Auth
+    auth = Auth()
 
 
 @app.errorhandler(404)
@@ -33,6 +37,28 @@ def forbidden(error) -> str:
     """ Forbidden handler
     """
     return jsonify({"error": "Forbidden"}), 403
+
+
+@app.before_request
+def before_request_func() -> str:
+    """ Before request handler
+    """
+    # Check if auth is None and return.
+    if auth is None:
+        return
+    # Define excluded paths.
+    excluded_paths = ['/api/v1/status/',
+                      '/api/v1/unauthorized/',
+                      '/api/v1/forbidden/']
+    # Check if path is excluded. If so, return.
+    if not auth.require_auth(request.path, excluded_paths):
+        return
+    # Check if auth header is None. If so, abort with 401.
+    if auth.authorization_header(request) is None:
+        abort(401)
+    # Check if current user is None. If so, abort with 403.
+    if auth.current_user(request) is None:
+        abort(403)
 
 
 if __name__ == "__main__":
