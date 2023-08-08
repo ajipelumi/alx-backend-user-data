40c95c68d7dc871a544d2b3be4644108ec301ec2
ajipelumi <ajisafeoluwapelumi@gmail.com>
2023-08-08 19:12:38 +0100

    handled decoding

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """ Basic Auth module. """
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -19,3 +20,24 @@ class BasicAuth(Auth):
             return None
         # Return authorization_header without Basic.
         return authorization_header[6:]
+
+    def decode_base64_authorization_header(self,
+                                           base64_authorization_header:
+                                           str) -> str:
+        """ Decode base64 authorization header. """
+        # Check if base64_authorization_header is None.
+        if base64_authorization_header is None:
+            return None
+        # Check if base64_authorization_header is not a string.
+        if not isinstance(base64_authorization_header, str):
+            return None
+        # Try to decode base64_authorization_header.
+        try:
+            base64_decoded = base64.b64decode(base64_authorization_header)
+        except Exception:
+            # If decoding fails, return None.
+            return None
+        # Decode decoded bytes to utf-8.
+        base64_string = base64_decoded.decode('utf-8')
+        # Return base64_string.
+        return base64_string
