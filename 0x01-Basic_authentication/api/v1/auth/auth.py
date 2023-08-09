da6cebce821c94d88ead6d1095326d0a01c1f032
ajipelumi <ajisafeoluwapelumi@gmail.com>
2023-08-09 02:00:57 +0100

    improved require auth method

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -8,12 +8,26 @@ class Auth:
     """ Auth class. """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """ Require auth. """
+        # Check if path is None or excluded_paths is None or empty.
         if path is None or excluded_paths is None or excluded_paths == []:
             return True
+        # Check if path ends with /.
+        # Add / to path if it does not end with / to ensure strict matching.
         if path[-1] != '/':
             path += '/'
+        # Handle excluded_paths ending with *.
+        for excluded in excluded_paths:
+            # If excluded ends with *.
+            if excluded.endswith('*/'):
+                # Remove * from excluded.
+                excluded_prefix = excluded[:-2]
+                # Check if path starts with excluded up to the *.
+                if path.startswith(excluded_prefix):
+                    return False
+        # Check if path is in excluded_paths.
         if path in excluded_paths:
             return False
+        # If path is not in excluded_paths, return True.
         return True
 
     def authorization_header(self, request=None) -> str:
