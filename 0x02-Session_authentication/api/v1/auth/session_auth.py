6de829ed6189529165b2e72d605ba319df30bef1
ajipelumi <ajisafeoluwapelumi@gmail.com>
2023-08-11 00:03:00 +0100

    improved session auth class

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -22,7 +22,7 @@ class SessionAuth(Auth):
         self.user_id_by_session_id[session_id] = user_id
         # Return session_id.
         return session_id
-    
+
     def user_id_for_session_id(self, session_id: str = None) -> str:
         """ User ID for session ID. """
         # Check if session_id is None.
