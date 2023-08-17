6bf676c0569b979e8b4633eb1fda252d58a5be2f
ajipelumi <ajisafeoluwapelumi@gmail.com>
2023-08-17 22:34:36 +0100

    handled sessions endpoint

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """ Flask app module. """
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort
 from auth import Auth
 
 
@@ -37,6 +37,32 @@ def register_user() -> str:
     except ValueError:
         # User already registered if ValueError
         return jsonify({"message": "email already registered"}), 400
+
+
+@app.route('/sessions', methods=['POST'], strict_slashes=False)
+def log_in() -> str:
+    """ POST /sessions
+    JSON body:
+      - email
+      - password
+      Return:
+      - Logged in user.
+    """
+    # Get the JSON
+    email = request.form.get('email')
+    password = request.form.get('password')
+    # Check if the email and password match
+    if AUTH.valid_login(email, password):
+        # Create a new session ID
+        session_id = AUTH.create_session(email)
+        # Create response JSON body
+        response = jsonify({"email": "{}".format(email),
+                            "message": "logged in"})
+        # Set the cookie
+        response.set_cookie("session_id", session_id)
+        return response
+    else:
+        abort(401)
 
 
 if __name__ == "__main__":
