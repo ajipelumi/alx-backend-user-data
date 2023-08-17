2b1daec408d3bd7678136bb99e7ee8c4f9d5b535
ajipelumi <ajisafeoluwapelumi@gmail.com>
2023-08-17 21:38:50 +0100

    implemented new user method

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,6 +1,28 @@
 #!/usr/bin/env python3
 """ Auth module. """
 import bcrypt
+from db import DB
+from user import User
+from sqlalchemy.orm.exc import NoResultFound
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """ Registers and returns a new user if email isn't listed. """
+        try:
+            self._db.find_user_by(email=email)
+            raise ValueError("User {} already exists".format(email))
+        except NoResultFound:
+            # Create a new user
+            hashed_password = _hash_password(password)
+            user = self._db.add_user(email, hashed_password)
+            return user
 
 
 def _hash_password(password: str) -> bytes:
