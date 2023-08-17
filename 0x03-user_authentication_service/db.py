0d7605e43c14e6cba7e6e421c23ce3581fec0b68
ajipelumi <ajisafeoluwapelumi@gmail.com>
2023-08-17 19:32:43 +0100

    setup db class

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -1,4 +1,4 @@
-##!/usr/bin/env python3
+#!/usr/bin/env python3
 """ DB module. """
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
@@ -12,7 +12,7 @@ class DB:
     """ DB class. """
     def __init__(self) -> None:
         """ Initialize a new DB instance. """
-        self._engine = create_engine("sqlite:///a.db", echo=True)
+        self._engine = create_engine("sqlite:///a.db")
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
