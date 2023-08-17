769ce17375641db788f1f521de673033b097c721
ajipelumi <ajisafeoluwapelumi@gmail.com>
2023-08-17 19:47:13 +0100

    handled find user method

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -4,6 +4,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
 from user import User
 from user import Base
 
@@ -30,4 +32,17 @@ class DB:
         user = User(email=email, hashed_password=hashed_password)
         self._session.add(user)
         self._session.commit()
+        return user
+
+    def find_user_by(self, **kwargs) -> User:
+        """ Finds a user instance. """
+        # Check if kwargs are valid columns
+        for key in kwargs.keys():
+            if not hasattr(User, key):
+                raise InvalidRequestError
+        # Query for user
+        user = self._session.query(User).filter_by(**kwargs).first()
+        if user is None:
+            raise NoResultFound
+        # Return user
         return user
